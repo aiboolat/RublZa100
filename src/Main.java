ff561b7180c02062331f92e6b40736b7a06a4609
Aybulat <ayba.irgalin@gmail.com>
2023-02-25 20:57:12 +0600

    ready

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 1100;
+                if (x>1000) {
+                    System.out.println(x/100+x);
+                }else {
+                    System.out.println(x);
+                }
+    }
+}
