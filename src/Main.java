53a8489f9eaebdea73dba5b239af83817b97050c
Aybulat <ayba.irgalin@gmail.com>
2023-02-26 19:00:02 +0600

    IspravilVse

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
-        int intrduced = 1100;
-        if (intrduced > 1000) {
-            System.out.println(intrduced / 100 + intrduced);
+        int initialAmount = 100;
+        int introduced = 1500;
+        if (introduced > 1000) {
+            System.out.println("Ваш баланс равен " + (introduced / 100 + initialAmount + introduced));
         } else {
-            System.out.println(intrduced);
+            System.out.println(introduced + initialAmount);
         }
+        System.out.println("Из них бонусов " + introduced / 100);
     }
 }
