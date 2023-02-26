5b841235b446d23fa7881b4ec47049bc3baf8a74
Aybulat <ayba.irgalin@gmail.com>
2023-02-26 12:51:57 +0600

    ispravlenie

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 1100;
-                if (x>1000) {
-                    System.out.println(x/100+x);
-                }else {
-                    System.out.println(x);
-                }
+        int intrduced = 1100;
+        if (intrduced > 1000) {
+            System.out.println(intrduced / 100 + intrduced);
+        } else {
+            System.out.println(intrduced);
+        }
     }
 }
